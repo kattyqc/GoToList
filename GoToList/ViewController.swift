ff613150d7e60466e2f33c51110153e5257c0ea6
Katty Quintero <kattyquinteroc@hotmail.com>
2021-08-29 17:36:57 +0200

    Initial Commit

---
+++ GoToList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoToList
+//
+//  Created by Katty Quintero on 29/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

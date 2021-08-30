ae1861fe06298afb49d8ad32be9bd7ece2e6dbf7
Katty Quintero <kattyquinteroc@hotmail.com>
2021-08-30 18:38:23 +0200

    Initial Commit

---
+++ CategoryTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryTableViewController.swift
+//  GoToList
+//
+//  Created by Katty Quintero on 30/8/21.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}

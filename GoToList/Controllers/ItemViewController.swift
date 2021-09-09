ef8a80ff50ad0a75d94c0447149671913173f9ba
Katty Quintero <kattyquinteroc@hotmail.com>
2021-09-09 15:52:32 +0200

    Established function in button search and at did select Row

--- GoToList/Controllers/ItemViewController.swift
+++ GoToList/Controllers/ItemViewController.swift
@@ -21,6 +21,8 @@ class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDe
     override func viewDidLoad() {
         super.viewDidLoad()
         loadItems()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
       
     }
 //MARRK: - Data Source UITableViewController
@@ -34,7 +36,18 @@ class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         cell.textLabel?.text =  itemArray[indexPath.row].name
         
+        cell.accessoryType = itemArray[indexPath.row].done == true ? .checkmark : .none
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveContext()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+
     }
     
 //MARK: - Manipulation data Methods
@@ -49,8 +62,16 @@ class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDe
     tableView.reloadData()
     }
 
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
 
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", detailCategories!.name! )
+        
+        if let additionPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
@@ -71,6 +92,7 @@ class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDe
             
             let newItem = Item(context: self.context)
             newItem.name = textField.text!
+            newItem.done = false
             newItem.parentCategory = self.detailCategories
             self.itemArray.append(newItem)
             
@@ -87,3 +109,29 @@ class ItemViewController: UIViewController, UITableViewDataSource, UITableViewDe
         present(alert, animated: true, completion: nil)
     }
 }
+    //MARK: - Search Bar Methods
+extension ItemViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "name CONSTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        
+        loadItems(with: request, predicate: request.predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
+
